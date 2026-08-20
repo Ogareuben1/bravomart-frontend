7ed3ebc2808d7003738c0f8716f08ca7cb2544d3
OGA REUBEN EKA <oga.eka@miva.edu.ng>
2026-08-20 16:58:20 +0100

    modified:   index.html

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/bravomart-frontend/',
   css: {
     postcss: './postcss.config.cjs',
   },
